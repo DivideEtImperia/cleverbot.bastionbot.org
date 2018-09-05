cc8253a101829b7c9769cf4c152330c48b99777d
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-09-05 17:52:38 +0000

    routes: update api router to use write method

--- source/routes/api.ts
+++ source/routes/api.ts
@@ -1,14 +1,23 @@
 "use strict";
 
 import * as express from "express";
+import write from "../methods/write";
 
 module Route {
   export class API {
     public async main(_req: express.Request, res: express.Response, next: express.NextFunction) {
       try {
-        res.json({
-          "Bastion": "Cleverbot API"
-        });
+        if (_req.query && _req.query.message) {
+          let reply: object = await write(decodeURIComponent(_req.query.message))
+
+          res.json(reply);
+        }
+        else {
+          res.json({
+            status: "success",
+            response: "Beep. Beep. Boop. Boop."
+          });
+        }
       }
       catch (e) {
         next(e);
