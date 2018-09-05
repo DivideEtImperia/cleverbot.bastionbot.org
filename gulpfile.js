204108f4d5b310e054943e20d09c389eeeac7d19
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-09-05 14:02:16 +0000

    gulp: add clean & compile task and remove default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const tsProject = ts.createProject('tsconfig.json');
+const del = require('del');
 
-gulp.task('default', () => {
+gulp.task('clean', () => {
+  // Clean `build` directory
+  return del([ 'build/*' ]);
+});
+
+gulp.task('compile', () => {
+  // Compine TypeScript
   return tsProject.src()
     .pipe(tsProject())
     .js.pipe(gulp.dest('build'));
